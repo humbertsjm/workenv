d4e1830a02123d12e3dcb8813871988b188f10b8
Humberto Vazquez <humbertosjm@proton.me>
2025-01-10 17:07:40 -0800

    create menu files

---
+++ src/menu/menu.py
@@ -0,0 +1,9 @@
+from typing import Self
+from src.menu.menu_helper import MenuHelper
+
+class Menu():
+    def __init__(self: Self):
+        pass
+
+    def main(self: Self):
+        pass
---
+++ src/menu/menu_helper.py
@@ -0,0 +1,10 @@
+from typing import Self
+
+
+class MenuHelper():
+    @classmethod
+    def get_menu_items(cls: Self) -> str:
+        message: str = ""
+        
+        return message
+    
